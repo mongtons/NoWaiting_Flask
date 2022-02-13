cc9564faf5e4ad98f4a9daa0ae9a08de9297ea49
mongtons <mongtons990213@gmail.com>
2022-02-13 22:35:42 +0900

    update flask python files

--- application/config.py
+++ application/config.py
@@ -3,5 +3,5 @@ from pybo.views.main_views import result
 
 BASE_DIR=os.path.dirname(__file__)
 db=result
-SQLALCHEMY_DATABASE_URI='mysql+pymysql:///root:kjh13579@localhost:3306/{}'.format(result)
+SQLALCHEMY_DATABASE_URI='mysql+pymysql:///root:kjh13579@localhost:3306/{}'.format(db)
 SQLALCHEMY_TRACK_MODIFICATIONS=False
--- application/pybo/views/main_views.py
+++ application/pybo/views/main_views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from models import Store
 
 bp=Blueprint('main', __name__, url_prefix='/')
 
